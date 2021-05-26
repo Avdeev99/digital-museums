444645db041f1649783acb2d9f76345000cfb28c
Oleksii Avdieiev <Oleksii_Avdieiev@epam.com>
2021-05-26 19:28:50 +0300

    Added deployment config

--- DigitalMuseums-UI/src/environments/environment.prod.ts
+++ DigitalMuseums-UI/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://localhost:5001',
+  // baseUrl: 'https://localhost:5001',
+  baseUrl: 'https://digital-museums-api.azurewebsites.net',
   googleClientId: '322648908927-qm312mal9bsgmmd7gd5fj99s9ie7btji.apps.googleusercontent.com',
   stripePk: 'pk_test_VzswUUouvRKh2mTzQSDvjsRf00VYoPHCi8',
   locales: ['en', 'ua'],
